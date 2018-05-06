5ed57a08a00d7ba4031e6bdd91e0388f0195a37b
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2018-05-05 22:15:23 -0700

    Initialization of environment

--- start.sh
+++ start.sh
@@ -3,8 +3,14 @@
 echo 127.0.0.1 `hostname` >> /etc/hosts
 
 # Start up the ROS server
-cd catkin_ws
-source devel/setup.bash
+source /opt/ros/kinetic/setup.bash
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/huegli/dotfiles
+ln -s dotfiles/vimrc .vimrc
+ln -s dotfiles/dir_colors .dir_colors
+ln -s dotfiles/tmux.conf .tmux.conf
+
 roscore &
-sleep 3
-avahi-browse -a | grep IPv4
+# sleep 3
+# avahi-browse -a | grep IPv4
