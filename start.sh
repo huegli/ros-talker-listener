bd20dcb8e4bfecc485ffc1d2522e0f887736c3e7
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2018-05-10 08:16:25 -0700

    Initi my_ws

--- start.sh
+++ start.sh
@@ -5,13 +5,17 @@ echo 127.0.0.1 `hostname` >> /etc/hosts
 # Start up the ROS server
 source /opt/ros/kinetic/setup.bash
 
-cd /root
+cd /ros/my_ws
+catkin_make
+
+cd ~
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/huegli/dotfiles
 ln -s dotfiles/vimrc .vimrc
 ln -s dotfiles/dir_colors .dir_colors
 ln -s dotfiles/tmux.conf .tmux.conf
+echo "source /ros/my_ws/devel/setup.bash" >> ~/.bashrc
 
 roscore
 # sleep 3
