231bf248cf39f5ac8ea89e651ed40b0d9be568d7
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2018-05-12 15:36:06 -0700

    Set up vim properly

--- start.sh
+++ start.sh
@@ -11,11 +11,16 @@ catkin_make
 cd ~
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
 git clone https://github.com/huegli/dotfiles
 ln -s dotfiles/vimrc .vimrc
 ln -s dotfiles/dir_colors .dir_colors
 ln -s dotfiles/tmux.conf .tmux.conf
 echo "source /ros/my_ws/devel/setup.bash" >> ~/.bashrc
+echo "export TERM=xterm-256color" >> ~/.bashrc
+
+export TERM=xterm-256color
+vim +PluginInstall +qall
 
 roscore
 # sleep 3
